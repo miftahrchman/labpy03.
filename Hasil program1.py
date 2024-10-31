e1a97227814cf108c632c6574a266ee9970ff531
miftahrchman <miftahurrochman13@gmail.com>
2024-10-31 06:14:24 -0700

    Create Hasil program1.py

---
+++ Hasil program1.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/user/Documents/kuliah upb/Sem 3/Program Komputer/pertemuan 6/program1.py 
+Laba bulan ke-1 sebesar: 0.00
+Laba bulan ke-2 sebesar: 0.00
+Laba bulan ke-3 sebesar: 1,000,000.00
+Laba bulan ke-4 sebesar: 1,000,000.00
+Laba bulan ke-5 sebesar: 1,050,000.00
+Laba bulan ke-6 sebesar: 1,050,000.00
+Laba bulan ke-7 sebesar: 1,050,000.00
+Laba bulan ke-8 sebesar: 31,500.00
+Total laba adalah: 5,181,500.00
+>>> 
