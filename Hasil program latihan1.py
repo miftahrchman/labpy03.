ba071e6db6ba34daf86f417ed701ec1d630bfa1d
miftahrchman <miftahurrochman13@gmail.com>
2024-10-31 06:04:46 -0700

    Create Hasil program latihan1.py

---
+++ Hasil program latihan1.py
@@ -0,0 +1,12 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/user/Documents/kuliah upb/Sem 3/Program Komputer/pertemuan 6/latihan 1/latihan1.py 
+Masukkan jumlah bilangan acak yang ingin ditampilkan: 5
+Data ke : 1 => 0.17079473290377056
+Data ke : 2 => 0.49860161892224386
+Data ke : 3 => 0.11116002573794237
+Data ke : 4 => 0.19209864575878732
+Data ke : 5 => 0.3209471565339379
+Selesai
+>>> 
