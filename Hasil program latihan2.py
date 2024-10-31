f2dbbe39733e79310ed2bc5cd49a25e7df9911d9
miftahrchman <miftahurrochman13@gmail.com>
2024-10-31 06:12:56 -0700

    Create Hasil program latihan2.py

---
+++ Hasil program latihan2.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/user/Documents/kuliah upb/Sem 3/Program Komputer/pertemuan 6/Latihan2.py 
+Masukkan angka (masukkan 0 untuk berhenti):
+Masukkan bilangan: 20
+Masukkan bilangan: 35
+Masukkan bilangan: 115
+Masukkan bilangan: 6
+Masukkan bilangan: 73
+Masukkan bilangan: 42
+Masukkan bilangan: 0
+Bilangan terbesar adalah: 115
+>>> 
